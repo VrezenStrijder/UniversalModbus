28b6fafcd244aa1bc5c3a493f5ae7f53884225f9
VrezenStrijder <f94fbr@outlook.com>
2025-07-29 19:01:59 +0800

    Update TcpConfig.cs

--- UMClient/Models/TcpConfig.cs
+++ UMClient/Models/TcpConfig.cs
@@ -6,22 +6,23 @@ using System.Threading.Tasks;
 
 namespace UMClient.Models
 {
-    public class TcpClientConfig
-    {
-        public string ServerAddress { get; set; } = "127.0.0.1";
-
-        public int ServerPort { get; set; } = 9016;
-
-        public int ConnectTimeout { get; set; } = 5000; // 连接超时(ms)
-    }
-
     public class TcpServerConfig
     {
         public string ListenAddress { get; set; } = "0.0.0.0";
 
-        public int ListenPort { get; set; } = 9006;
+        public int ListenPort { get; set; } = 9010;
 
         public int MaxClients { get; set; } = 20;
     }
+
+    public class TcpClientConfig
+    {
+        public string ServerAddress { get; set; } = "127.0.0.1";
+
+        public int ServerPort { get; set; } = 9010;
+
+        public int ConnectTimeout { get; set; } = 5000; // 连接超时(ms)
+    }
+
 
 }
