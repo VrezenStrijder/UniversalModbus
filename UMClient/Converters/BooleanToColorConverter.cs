d8e6eb450d0aef6acec27e125d261e5e37830b34
VrezenStrijder <f94fbr@outlook.com>
2025-08-06 18:28:51 +0800

    Create BooleanToColorConverter.cs

---
+++ UMClient/Converters/BooleanToColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace UMClient.Converters
+{
+    public class BooleanToColorConverter : IValueConverter
+    {
+        public static readonly BooleanToColorConverter Instance = new();
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue && parameter is string colorPair)
+            {
+                var colors = colorPair.Split('|');
+                if (colors.Length == 2)
+                {
+                    var colorString = boolValue ? colors[1] : colors[0];
+                    return SolidColorBrush.Parse(colorString);
+                }
+            }
+            return Brushes.White;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
